f2d2f476a2c5c1c2a65d85f793f65fc39a49e5f2
CedricGiese <cedric.giese@stud.uni-due.de>
2023-05-06 17:49:38 +0200

    removed .meta files

--- Assets/scripts/Wichtel.cs
+++ Assets/scripts/Wichtel.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static ResourceEnum;
+using ResourceNameSpace;
 
 public class Wichtel : MonoBehaviour, Clickable
 {
